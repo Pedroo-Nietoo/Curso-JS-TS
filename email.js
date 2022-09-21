6a31aabbaa5efcab406d55a4fab5c0b930decf01
Pedro Nieto <pedronieto.2005@gmail.com>
2022-09-20 21:27:38 -0300

    Create email.js

---
+++ email.js
@@ -0,0 +1,7 @@
+let emailRuim = " pedroO-Nietoo@mEuemail.com"
+function formataEmail(email){
+  email = email.toLowerCase()
+  email = email.trim()
+  return email
+}
+console.log(email) //pedroo-nietoo@meuemail.com
