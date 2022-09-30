7a1175e90b30cc4209fb71998a8ae06827aed76d
Pedro Nieto <pedronieto.2005@gmail.com>
2022-09-30 20:12:40 -0300

    Create reduceNaMatriz.js

---
+++ SENAI/reduceNaMatriz.js
@@ -0,0 +1,7 @@
+function somaNumeros(numeros) {
+  let soma = numeros.reduce(function(total, atual){
+    return total + atual
+  }, 0)
+  return soma
+}
+console.log(somaNumeros([10, 20, 30])) // 60
